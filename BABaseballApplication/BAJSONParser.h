04372cc195c1423b7fca183d71cbd9e7f81fb620
Mac <mac@macbook-pro-4.local>
2015-09-26 18:25:17 -0400

    Whatever

---
+++ BABaseballApplication/BAJSONParser.h
@@ -0,0 +1,15 @@
+//
+//  BAJSONParser.h
+//  BABaseballApplication
+//
+//  Created by Mac on 9/26/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BAJSONParser : NSObject
+
+- (NSDictionary *)parseJSON;
+
+@end
---
+++ BABaseballApplication/BAJSONParser.m
@@ -0,0 +1,20 @@
+//
+//  BAJSONParser.m
+//  BABaseballApplication
+//
+//  Created by Mac on 9/26/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import "BAJSONParser.h"
+
+@implementation BAJSONParser
+
+-(NSDictionary*)parseJSON {
+    NSDictionary *jsonDictionary = [[NSDictionary alloc] init];
+    NSURL        *url;
+    
+    return jsonDictionary;
+}
+
+@end
