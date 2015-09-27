76497798449e16079324cff6d0aef27f142746dc
Mac <mac@macbook-pro-4.local>
2015-09-27 14:18:43 -0400

    Whatever

--- BABaseballApplication/ViewController.m
+++ BABaseballApplication/ViewController.m
@@ -15,22 +15,14 @@
 
 @implementation ViewController
 
-/**
- *  Method - viewDidLoad
- */
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     BAJSONParser *jsonParser = [[BAJSONParser alloc] init];
     [jsonParser parseJSON:@"2015" andMonth:@"09" andDay:@"26"];
 }
 
-/**
- *  Method - didReceiveMemoryWarning
- */
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
