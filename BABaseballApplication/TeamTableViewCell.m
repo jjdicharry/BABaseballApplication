898ce9ff4feeab40296ac493e87e84784c5b71a1
jjdicharry <jjdicharry@gmail.com>
2015-09-28 12:50:26 -0400

    Created table view for teams.

---
+++ BABaseballApplication/TeamTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  TeamTableViewCell.m
+//  BABaseballApplication
+//
+//  Created by Mac on 9/28/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import "TeamTableViewCell.h"
+
+@implementation TeamTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (IBAction)favoritePressed:(id)sender {
+}
+
+@end
