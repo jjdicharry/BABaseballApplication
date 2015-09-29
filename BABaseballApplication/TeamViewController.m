f6e0b7b34b537dd909f523762a87d161064f342c
jjdicharry <jjdicharry@gmail.com>
2015-09-28 20:37:41 -0400

    Added Defaults.plist for teams table view.

--- BABaseballApplication/TeamViewController.m
+++ BABaseballApplication/TeamViewController.m
@@ -27,9 +27,13 @@
     BAJSONParser *jsonParser = [[BAJSONParser alloc] init];
     [jsonParser parseJSON:@"2015" andMonth:@"09" andDay:@"26"];
 	
-	teams = [[NSMutableArray alloc] init];
-	[teams addObject:[[Team alloc] initWithName:@"Chargers"]];
-	[teams addObject:[[Team alloc] initWithName:@"Packers"]];
+	// load teams from Defaults.plist
+	NSBundle *bundle = [NSBundle mainBundle];
+	NSString *path;
+	NSMutableDictionary *root;
+	path = [bundle pathForResource:@"Defaults" ofType:@"plist"];
+	root = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+	teams = [root objectForKey:@"Teams"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,21 +41,35 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+#pragma mark - UITableViewDelegate Methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+	return @"Section Title";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
 {
 	return teams.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (UITableViewCell *)tableView:(UITableView *)tableView
+	 cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	static NSString *reuseIdentifier = @"TeamTableViewCell";
 	TeamTableViewCell *cell;
-	Team * team;
+	NSDictionary *team;
 	
 	cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
 	team = [teams objectAtIndex:indexPath.row];
 	
-	cell.teamNameLabel.text = team.name;
+	cell.teamNameLabel.text = [team objectForKey:@"Name"];
 	
 	return cell;
 }
