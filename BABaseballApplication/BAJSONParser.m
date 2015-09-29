89faecb72e0b2e4a94bf3e9b012e872370b0f8ec
Mac <mac@macbook-pro-4.local>
2015-09-28 20:01:04 -0400

    Whatever

--- BABaseballApplication/BAJSONParser.m
+++ BABaseballApplication/BAJSONParser.m
@@ -51,68 +51,124 @@
         NSDictionary *winnPitcher     = [gameDictionary objectForKey:@"winning_pitcher"];
         NSDictionary *losePitcher     = [gameDictionary objectForKey:@"losing_pitcher"];
         NSDictionary *lineScore       = [gameDictionary objectForKey:@"linescore"];
-        NSDictionary *homeRunDic      = [lineScore      objectForKey:@"hr"];
+        NSDictionary *run             = [lineScore      objectForKey:@"r"];
+        NSDictionary *hit             = [lineScore      objectForKey:@"h"];
         NSDictionary *error           = [lineScore      objectForKey:@"e"];
         NSDictionary *strikeOut       = [lineScore      objectForKey:@"so"];
         NSDictionary *stoleBase       = [lineScore      objectForKey:@"sb"];
-        NSDictionary *run             = [lineScore      objectForKey:@"r"];
-        NSDictionary *hit             = [lineScore      objectForKey:@"h"];
+        NSDictionary *homeRunDic      = [lineScore      objectForKey:@"hr"];
+        NSArray      *inning          = [lineScore      objectForKey:@"inning"];
         BAScoreboard *scoreboard      = [[BAScoreboard alloc] init];
         
-        scoreboard.gameDate           = [gameDictionary objectForKey:@"original_date"];
-        scoreboard.location           = [gameDictionary objectForKey:@"location"];
-        scoreboard.time               = [gameDictionary objectForKey:@"time"];
-        scoreboard.amPM               = [gameDictionary objectForKey:@"ampm"];
-        scoreboard.venue              = [gameDictionary objectForKey:@"venue"];
-        scoreboard.awayTeamName       = [gameDictionary objectForKey:@"away_team_name"];
-        scoreboard.awayTeamCity       = [gameDictionary objectForKey:@"away_team_city"];
-        scoreboard.awayTeamAbbr       = [gameDictionary objectForKey:@"away_name_abbrev"];
-        scoreboard.awayWin            = [gameDictionary objectForKey:@"away_win"];
-        scoreboard.awayLoss           = [gameDictionary objectForKey:@"away_loss"];
-        scoreboard.awayGameBack       = [gameDictionary objectForKey:@"away_games_back"];
-        scoreboard.awayGameBackWC     = [gameDictionary objectForKey:@"away_games_back_wildcard"];
-        scoreboard.homeTeamName       = [gameDictionary objectForKey:@"home_team_name"];
-        scoreboard.homeTeamCity       = [gameDictionary objectForKey:@"home_team_city"];
-        scoreboard.homeTeamAbbr       = [gameDictionary objectForKey:@"home_name_abbrev"];
-        scoreboard.homeWin            = [gameDictionary objectForKey:@"home_win"];
-        scoreboard.homeLoss           = [gameDictionary objectForKey:@"home_loss"];
-        scoreboard.homeGameBack       = [gameDictionary objectForKey:@"home_games_back"];
-        scoreboard.homeGameBackWC     = [gameDictionary objectForKey:@"home_games_back_wildcard"];
-        scoreboard.inning             = [lineScore   objectForKey:@"inning"];
-        scoreboard.savePitchLastName  = [savePitcher objectForKey:@"last"];
-        scoreboard.savePitchFirstName = [savePitcher objectForKey:@"first"];
-        scoreboard.savePitchNumber    = [savePitcher objectForKey:@"number"];
-        scoreboard.savePitchWin       = [savePitcher objectForKey:@"wins"];
-        scoreboard.savePitchSave      = [savePitcher objectForKey:@"saves"];
-        scoreboard.savePitchLoss      = [savePitcher objectForKey:@"losses"];
-        scoreboard.savePitchERA       = [savePitcher objectForKey:@"era"];
-        scoreboard.winnPitchLastName  = [winnPitcher objectForKey:@"last"];
-        scoreboard.winnPitchFirstName = [winnPitcher objectForKey:@"first"];
-        scoreboard.winnPitchNumber    = [winnPitcher objectForKey:@"number"];
-        scoreboard.winnPitchWin       = [winnPitcher objectForKey:@"wins"];
-        scoreboard.winnPitchSave      = [winnPitcher objectForKey:@"saves"];
-        scoreboard.winnPitchLoss      = [winnPitcher objectForKey:@"losses"];
-        scoreboard.winnPitchERA       = [winnPitcher objectForKey:@"era"];
-        scoreboard.losePitchLastName  = [losePitcher objectForKey:@"last"];
-        scoreboard.losePitchFirstName = [losePitcher objectForKey:@"first"];
-        scoreboard.losePitchNumber    = [losePitcher objectForKey:@"number"];
-        scoreboard.losePitchWin       = [losePitcher objectForKey:@"wins"];
-        scoreboard.losePitchSave      = [losePitcher objectForKey:@"saves"];
-        scoreboard.losePitchLoss      = [losePitcher objectForKey:@"losses"];
-        scoreboard.losePitchERA       = [losePitcher objectForKey:@"era"];
-        scoreboard.awayHomeRun        = [homeRunDic  objectForKey:@"away"];
-        scoreboard.awayError          = [error       objectForKey:@"away"];
-        scoreboard.awayStrikeOut      = [strikeOut   objectForKey:@"away"];
-        scoreboard.awayStoleBase      = [stoleBase   objectForKey:@"away"];
-        scoreboard.awayRun            = [run         objectForKey:@"away"];
-        scoreboard.awayHit            = [hit         objectForKey:@"away"];
-        scoreboard.homeHomeRun        = [homeRunDic  objectForKey:@"home"];
-        scoreboard.homeError          = [error       objectForKey:@"home"];
-        scoreboard.homeStrikeOut      = [strikeOut   objectForKey:@"home"];
-        scoreboard.homeStoleBase      = [stoleBase   objectForKey:@"home"];
-        scoreboard.homeRun            = [run         objectForKey:@"home"];
-        scoreboard.homeHit            = [hit         objectForKey:@"home"];
+        scoreboard.gameDate           = [gameDictionary  objectForKey:@"original_date"];
+        scoreboard.location           = [gameDictionary  objectForKey:@"location"];
+        scoreboard.time               = [gameDictionary  objectForKey:@"time"];
+        scoreboard.amPM               = [gameDictionary  objectForKey:@"ampm"];
+        scoreboard.venue              = [gameDictionary  objectForKey:@"venue"];
+        scoreboard.awayTeamName       = [gameDictionary  objectForKey:@"away_team_name"];
+        scoreboard.awayTeamCity       = [gameDictionary  objectForKey:@"away_team_city"];
+        scoreboard.awayTeamAbbr       = [gameDictionary  objectForKey:@"away_name_abbrev"];
+        scoreboard.awayWin            = [[gameDictionary objectForKey:@"away_win"] integerValue];
+        scoreboard.awayLoss           = [[gameDictionary objectForKey:@"away_loss"] integerValue];
+        scoreboard.awayGameBack       = [NSDecimalNumber decimalNumberWithString:
+                                         [gameDictionary objectForKey:@"away_games_back"]];
+        scoreboard.awayGameBackWC     = [NSDecimalNumber decimalNumberWithString:
+                                         [gameDictionary objectForKey:@"away_games_back_wildcard"]];
+        scoreboard.awayRun            = [[run            objectForKey:@"away"] integerValue];
+        scoreboard.awayHit            = [[hit            objectForKey:@"away"] integerValue];
+        scoreboard.awayError          = [[error          objectForKey:@"away"] integerValue];
+        scoreboard.awayStrikeOut      = [[strikeOut      objectForKey:@"away"] integerValue];
+        scoreboard.awayStoleBase      = [[stoleBase      objectForKey:@"away"] integerValue];
+        scoreboard.awayHomeRun        = [[homeRunDic     objectForKey:@"away"] integerValue];
+        scoreboard.homeTeamName       = [gameDictionary  objectForKey:@"home_team_name"];
+        scoreboard.homeTeamCity       = [gameDictionary  objectForKey:@"home_team_city"];
+        scoreboard.homeTeamAbbr       = [gameDictionary  objectForKey:@"home_name_abbrev"];
+        scoreboard.homeWin            = [[gameDictionary objectForKey:@"home_win"] integerValue];
+        scoreboard.homeLoss           = [[gameDictionary objectForKey:@"home_loss"] integerValue];
+        scoreboard.homeGameBack       = [NSDecimalNumber decimalNumberWithString:
+                                         [gameDictionary objectForKey:@"home_games_back"]];
+        scoreboard.homeGameBackWC     = [NSDecimalNumber decimalNumberWithString:
+                                         [gameDictionary objectForKey:@"home_games_back_wildcard"]];
+        scoreboard.homeRun            = [[run            objectForKey:@"home"] integerValue];
+        scoreboard.homeHit            = [[hit            objectForKey:@"home"] integerValue];
+        scoreboard.homeError          = [[error          objectForKey:@"home"] integerValue];
+        scoreboard.homeStrikeOut      = [[strikeOut      objectForKey:@"home"] integerValue];
+        scoreboard.homeStoleBase      = [[stoleBase      objectForKey:@"home"] integerValue];
+        scoreboard.homeHomeRun        = [[homeRunDic     objectForKey:@"home"] integerValue];
+        scoreboard.savePitchLastName  = [savePitcher     objectForKey:@"last"];
+        scoreboard.savePitchFirstName = [savePitcher     objectForKey:@"first"];
+        scoreboard.savePitchNumber    = [[savePitcher    objectForKey:@"number"] integerValue];
+        scoreboard.savePitchWin       = [[savePitcher    objectForKey:@"wins"]   integerValue];
+        scoreboard.savePitchSave      = [[savePitcher    objectForKey:@"saves"]  integerValue];
+        scoreboard.savePitchLoss      = [[savePitcher    objectForKey:@"losses"] integerValue];
+        scoreboard.savePitchERA       = [NSDecimalNumber decimalNumberWithString:
+                                         [savePitcher    objectForKey:@"era"]];
+        scoreboard.winnPitchLastName  = [winnPitcher     objectForKey:@"last"];
+        scoreboard.winnPitchFirstName = [winnPitcher     objectForKey:@"first"];
+        scoreboard.winnPitchNumber    = [[winnPitcher    objectForKey:@"number"] integerValue];
+        scoreboard.winnPitchWin       = [[winnPitcher    objectForKey:@"wins"]   integerValue];
+        scoreboard.winnPitchSave      = [[winnPitcher    objectForKey:@"saves"]  integerValue];
+        scoreboard.winnPitchLoss      = [[winnPitcher    objectForKey:@"losses"] integerValue];
+        scoreboard.winnPitchERA       = [NSDecimalNumber decimalNumberWithString:
+                                         [winnPitcher    objectForKey:@"era"]];
+        scoreboard.losePitchLastName  = [losePitcher     objectForKey:@"last"];
+        scoreboard.losePitchFirstName = [losePitcher     objectForKey:@"first"];
+        scoreboard.losePitchNumber    = [[losePitcher    objectForKey:@"number"] integerValue];
+        scoreboard.losePitchWin       = [[losePitcher    objectForKey:@"wins"]   integerValue];
+        scoreboard.losePitchSave      = [[losePitcher    objectForKey:@"saves"]  integerValue];
+        scoreboard.losePitchLoss      = [[losePitcher    objectForKey:@"losses"] integerValue];
+        scoreboard.losePitchERA       = [NSDecimalNumber decimalNumberWithString:
+                                         [losePitcher    objectForKey:@"era"]];
 
+        int i = 0;
+        for (NSDictionary *inningDictionary in inning) {
+            NSString *away = [inningDictionary objectForKey:@"away"];
+            NSString *home = [inningDictionary objectForKey:@"home"];
+            
+            i++;
+            switch (i) {
+                case 1:
+                    scoreboard.awayInni01 = [away integerValue];
+                    scoreboard.homeInni01 = [home integerValue];
+                    break;
+                case 2:
+                    scoreboard.awayInni02 = [away integerValue];
+                    scoreboard.homeInni02 = [home integerValue];
+                    break;
+                case 3:
+                    scoreboard.awayInni03 = [away integerValue];
+                    scoreboard.homeInni03 = [home integerValue];
+                    break;
+                case 4:
+                    scoreboard.awayInni04 = [away integerValue];
+                    scoreboard.homeInni04 = [home integerValue];
+                    break;
+                case 5:
+                    scoreboard.awayInni05 = [away integerValue];
+                    scoreboard.homeInni05 = [home integerValue];
+                    break;
+                case 6:
+                    scoreboard.awayInni06 = [away integerValue];
+                    scoreboard.homeInni06 = [home integerValue];
+                    break;
+                case 7:
+                    scoreboard.awayInni07 = [away integerValue];
+                    scoreboard.homeInni07 = [home integerValue];
+                    break;
+                case 8:
+                    scoreboard.awayInni08 = [away integerValue];
+                    scoreboard.homeInni08 = [home integerValue];
+                    break;
+                case 9:
+                    scoreboard.awayInni09 = [away integerValue];
+                    scoreboard.homeInni09 = [home integerValue];
+                    break;
+                default:
+                    scoreboard.awayInniEx = [away integerValue];
+                    scoreboard.homeInniEx = [home integerValue];
+                    break;
+            }
+        }
         
     }
 }
