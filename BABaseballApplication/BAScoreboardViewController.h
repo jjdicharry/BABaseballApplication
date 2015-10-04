743b4e3ca0e9e349beb70b97156440e62316cbcb
Mac <mac@macbook-pro-4.local>
2015-10-04 12:49:12 -0400

    Whatever

---
+++ BABaseballApplication/BAScoreboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  BAScoreboardViewController.h
+//  BABaseballApplication
+//
+//  Created by Mac on 10/4/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BAScoreboardViewController : UIViewController
+
+@end
---
+++ BABaseballApplication/BAScoreboardViewController.m
@@ -0,0 +1,37 @@
+//
+//  BAScoreboardViewController.m
+//  BABaseballApplication
+//
+//  Created by Mac on 10/4/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import "BAScoreboardViewController.h"
+
+@interface BAScoreboardViewController ()
+
+@end
+
+@implementation BAScoreboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
