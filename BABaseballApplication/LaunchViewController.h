d47d6bc5d78217b31b7fb9d432c3b19980b2a282
jjdicharry <jjdicharry@gmail.com>
2015-10-06 20:12:39 -0400

    Added launch screen view controller class.

---
+++ BABaseballApplication/LaunchViewController.h
@@ -0,0 +1,14 @@
+//
+//  LaunchViewController.h
+//  BABaseballApplication
+//
+//  Created by Mac on 10/6/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchViewController : UIViewController
+	<UISplitViewControllerDelegate>
+
+@end
---
+++ BABaseballApplication/LaunchViewController.m
@@ -0,0 +1,37 @@
+//
+//  LaunchViewController.m
+//  BABaseballApplication
+//
+//  Created by Mac on 10/6/15.
+//  Copyright © 2015 MAC. All rights reserved.
+//
+
+#import "LaunchViewController.h"
+
+@interface LaunchViewController ()
+
+@end
+
+@implementation LaunchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
