ea6ecd2340c56ffcf4209ab1b4f640725c73b583
alfredotv <alfredotv@msn.com>
2015-10-08 00:19:33 -0400

    Alf

--- BABaseballApplication/TeamViewController.m
+++ BABaseballApplication/TeamViewController.m
@@ -33,29 +33,13 @@
     // Do any additional setup after loading the view, typically from a nib.
     BAJSON *json = [[BAJSON alloc] init];
     
-    for (int i=1; i<=7; i++) {
+/*
+    for (int i=1; i<=5; i++) {
         [json parseScoreboardWithYear:@"2015"
                              andMonth:@"10"
                                andDay:[NSString stringWithFormat:@"%02i",i]];
     }
-    
-    for (int i=1; i<=30; i++) {
-        [json parseScoreboardWithYear:@"2015"
-                             andMonth:@"09"
-                               andDay:[NSString stringWithFormat:@"%02i",i]];
-    }
-    
-    for (int i=1; i<=31; i++) {
-        [json parseScoreboardWithYear:@"2015"
-                             andMonth:@"08"
-                               andDay:[NSString stringWithFormat:@"%02i",i]];
-    }
-    
-    for (int i=1; i<=31; i++) {
-        [json parseScoreboardWithYear:@"2015"
-                             andMonth:@"07"
-                               andDay:[NSString stringWithFormat:@"%02i",i]];
-    }
+*/
     
     // load teams from Defaults.plist
     NSBundle *bundle = [NSBundle mainBundle];
