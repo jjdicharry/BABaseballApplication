cd46ba26aead67b0c0866d0a17f078612e6b40d3
alfredotv <alfredotv@msn.com>
2015-10-08 00:28:38 -0400

    What

--- BABaseballApplication/TeamViewController.m
+++ BABaseballApplication/TeamViewController.m
@@ -31,9 +31,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+/*
     BAJSON *json = [[BAJSON alloc] init];
     
-/*
     for (int i=1; i<=5; i++) {
         [json parseScoreboardWithYear:@"2015"
                              andMonth:@"10"
